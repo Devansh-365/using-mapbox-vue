191a7259321e9386a475d803ec723e3f5909b250
Devansh Tiwari <devanshtiwari365@gmail.com>
2022-07-24 02:02:34 +0530

    :tada: Starting..
    USing mapbox & vue

---
+++ pages/map.vue
@@ -0,0 +1,14 @@
+<template>
+    <MapboxMap
+    ref="mapboxMap"
+    access-token="pk.eyJ1IjoiZGV2YW5zaDM2NSIsImEiOiJjbDV5YnV2dW0wY2VlM2RwMjYzZzY1anFyIn0.5njP9f5rkA5KNHRmlALP_A" />
+</template>
+
+<script>
+  import { ref } from 'vue';
+  import { MapboxMap } from '@studiometa/vue-mapbox-gl';
+
+  const mapboxMap = ref();
+  const map = computed(() => mapboxMap.value.map);
+
+</script>
