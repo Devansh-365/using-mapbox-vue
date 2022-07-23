df28b5231aa42cdc6503fc1d70986ca4fadca902
Devansh Tiwari <devanshtiwari365@gmail.com>
2022-07-24 02:47:26 +0530

    :sparkles: Add map
    added map using mapboxgl

--- pages/map.vue
+++ pages/map.vue
@@ -1,14 +1,31 @@
 <template>
-    <MapboxMap
-    ref="mapboxMap"
-    access-token="pk.eyJ1IjoiZGV2YW5zaDM2NSIsImEiOiJjbDV5YnV2dW0wY2VlM2RwMjYzZzY1anFyIn0.5njP9f5rkA5KNHRmlALP_A" />
+    <div id="map" class="w-full h-screen"></div>
 </template>
-
 <script>
-  import { ref } from 'vue';
-  import { MapboxMap } from '@studiometa/vue-mapbox-gl';
 
-  const mapboxMap = ref();
-  const map = computed(() => mapboxMap.value.map);
-
+import mapboxgl from 'mapbox-gl'
+export default {
+    data(){
+        return{
+            access_token: 'pk.eyJ1IjoiZGV2YW5zaDM2NSIsImEiOiJjbDV5YnV2dW0wY2VlM2RwMjYzZzY1anFyIn0.5njP9f5rkA5KNHRmlALP_A',
+            map:{}
+        }
+    },
+    mounted(){
+            this.createMap()
+        },
+    methods: {
+        createMap(){
+            mapboxgl.accessToken = this.access_token
+            this.map = new mapboxgl.Map({
+                container: 'map',
+                style: 'mapbox://styles/mapbox/streets-v11',
+                zoom: 11,
+                center: [107.61861,-6.90389]
+            })
+        }
+    }
+}
 </script>
+
+</style>
